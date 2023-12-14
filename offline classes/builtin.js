86d9dba745e5f5f7243e29edff5f9f64486f66f0
junaidahmed-One <junaidahmed882000@gmail.com>
2023-12-14 20:49:54 +0530

    learnt about map,filter,arrow functions

---
+++ offline classes/builtin.js
@@ -0,0 +1,9 @@
+const input = [1, 2, 3, 4, 5];
+
+const input1 = [1, 2, 3, 4, 5];
+
+const ans = input.map((i) => i * 2);
+const ans1 = input1.filter((i) => i % 2 === 0);
+
+console.log(ans);
+console.log(ans1);
