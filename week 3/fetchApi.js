89deb841344c5404679b8037decf4c3b8f7449b5
junaidahmed-One <junaidahmed882000@gmail.com>
2023-12-18 14:52:57 +0530

    using fetch with async/await

--- week 3/fetchApi.js
+++ week 3/fetchApi.js
@@ -1,8 +1,16 @@
-fetch("https://fakerapi.it/api/v1/persons")
-	.then((res) => res.json())
-	.then((data) => {
-		console.log(data.data[0]);
-	})
-	.catch((error) => {
-		console.error("Error:", error);
-	});
+// fetch("https://fakerapi.it/api/v1/persons")
+// 	.then((res) => res.json())
+// 	.then((data) => {
+// 		console.log(data.data[0]);
+// 	})
+// 	.catch((error) => {
+// 		console.error("Error:", error);
+// 	});
+
+async function useFetch() {
+	const result = await fetch("https://fakerapi.it/api/v1/persons");
+	const res = await result.json();
+	console.log(res.data[0].address);
+}
+
+useFetch();
