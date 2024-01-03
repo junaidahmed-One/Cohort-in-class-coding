99bdf8b31a05ce0766dec3819ffab30ed42134a2
junaidahmed-One <junaidahmed882000@gmail.com>
2024-01-03 13:42:26 +0530

    added useEffect while fetching todos

--- week-5/new_year_todo_app/frontend/src/App.jsx
+++ week-5/new_year_todo_app/frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CreateTodo } from "./components/CreateTodo";
 import { Todos } from "./components/Todos";
 
 function App() {
 	const [todos, setTodos] = useState([]);
 
-	fetch("http://localhost:3000/todos").then(async function (res) {
-		const json = await res.json();
-		setTodos(json.todos);
-	});
+	useEffect(() => {
+		setInterval(() => {
+			fetch("http://localhost:3000/todos").then(async function (res) {
+				const json = await res.json();
+				setTodos(json.todos);
+			});
+		}, 10000);
+	}, [todos]);
 	return (
 		<div>
 			<CreateTodo></CreateTodo>
