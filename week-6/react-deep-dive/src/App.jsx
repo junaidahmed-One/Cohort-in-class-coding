552696b9d4cc2af9e5ccd787bc57b31768ae5846
junaidahmed-One <junaidahmed882000@gmail.com>
2024-01-06 20:33:40 +0530

    learnt about Wrapper components

--- week-6/react-deep-dive/src/App.jsx
+++ week-6/react-deep-dive/src/App.jsx
@@ -3,56 +3,26 @@ import { memo } from "react";
 
 let counter = 4;
 function App() {
-	const [todos, setTodos] = useState([
-		{
-			id: 1,
-			title: "Go to gym",
-			description: "Gym in the morning",
-		},
-		{
-			id: 2,
-			title: "Go to gym",
-			description: "Gym in the morning",
-		},
-		{
-			id: 3,
-			title: "Go to gym",
-			description: "Gym in the morning",
-		},
-	]);
-
-	function addTodo() {
-		setTodos([
-			...todos,
-			{
-				id: counter++,
-				title: Math.random(),
-				description: Math.random(),
-			},
-		]);
-	}
-
 	return (
 		<div>
-			<button onClick={addTodo}>Add Todo</button>
-			{todos.map((todo) => (
-				<Todo
-					key={todo.id}
-					title={todo.title}
-					description={todo.description}
-				></Todo>
-			))}
+			<CardWrapper>
+				<TextComponent></TextComponent>
+			</CardWrapper>
+			<CardWrapper>
+				<TextComponent></TextComponent>
+			</CardWrapper>
 		</div>
 	);
 }
 
-function Todo({ title, description }) {
+function CardWrapper({ children }) {
 	return (
-		<div>
-			<h1>{title}</h1>
-			<h4>{description}</h4>
-		</div>
+		<div style={{ border: "2px solid black", padding: 20 }}>{children}</div>
 	);
+}
+
+function TextComponent() {
+	return <div>Text Component</div>;
 }
 
 export default App;
