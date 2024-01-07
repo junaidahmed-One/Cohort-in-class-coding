555a533574914b8241bf49bfbb29deaccd73f9f2
junaidahmed-One <junaidahmed882000@gmail.com>
2024-01-07 20:13:31 +0530

    useMemo

--- week-6/react-deep-dive/src/App.jsx
+++ week-6/react-deep-dive/src/App.jsx
@@ -1,59 +1,35 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useMemo, useState } from "react";
 
 function App() {
-	const [selectedID, setID] = useState(1);
-	return (
-		<div>
-			<button
-				onClick={function () {
-					setID(1);
-				}}
-			>
-				1
-			</button>
-			<button
-				onClick={function () {
-					setID(2);
-				}}
-			>
-				2
-			</button>
-			<button
-				onClick={function () {
-					setID(3);
-				}}
-			>
-				3
-			</button>
-			<button
-				onClick={function () {
-					setID(4);
-				}}
-			>
-				4
-			</button>
-			<Todo id={selectedID} />
-		</div>
-	);
-}
+	const [counter, setCounter] = useState(0);
+	const [inputValue, setInputValue] = useState(1);
 
-function Todo({ id }) {
-	const [todo, setTodo] = useState({});
-
-	useEffect(() => {
-		fetch("https://sum-server.100xdevs.com/todo?id=" + id).then(
-			async function (res) {
-				const json = await res.json();
-				setTodo(json.todo);
-			}
-		);
-	}, [id]);
+	let count = useMemo(() => {
+		let count = 0;
+		for (let i = 1; i <= inputValue; i++) {
+			count = count + i;
+		}
+		return count;
+	}, [inputValue]);
 
 	return (
 		<div>
-			<h1>{todo.title}</h1>
-			<h4>{todo.description}</h4>
+			<input
+				onChange={function (e) {
+					setInputValue(e.target.value);
+				}}
+				placeholder={"Find sum from 1 to n"}
+			></input>
+			<br />
+			Sum from 1 to {inputValue} is {count}
+			<br />
+			<button
+				onClick={() => {
+					setCounter(counter + 1);
+				}}
+			>
+				Counter ({counter})
+			</button>
 		</div>
 	);
 }
