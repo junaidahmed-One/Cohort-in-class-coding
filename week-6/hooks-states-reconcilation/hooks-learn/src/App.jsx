2bba45b53a2373002ad2e484134f8c45439f7269
junaidahmed-One <junaidahmed882000@gmail.com>
2024-08-05 11:17:47 +0530

    useEffect and its use-case

--- week-6/hooks-states-reconcilation/hooks-learn/src/App.jsx
+++ week-6/hooks-states-reconcilation/hooks-learn/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [excahngeData, setExcahngeData] = useState({});
+	const [bankData, setBankData] = useState({});
+	console.log(`Re-rendered`);
 
-	return (
-		<div>
-			<button onClick={() => setCount((count) => count + 1)}>
-				Count is {count}
-			</button>
-		</div>
+	useEffect(
+		() => {
+			//what logic to run
+			setTimeout(() => {
+				setBankData({ income: 100 });
+			}, 3000);
+		},
+		[] /* Dependency array [] -> on what state variable change should it run*/
 	);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setExcahngeData({ returns: 100 });
+		}, 1000);
+	}, []);
+
+	const incomeTax = (bankData.income + excahngeData.returns) * 0.3;
+
+	return <div>Hi your income tax returns are {incomeTax}</div>;
 }
 
 export default App;
