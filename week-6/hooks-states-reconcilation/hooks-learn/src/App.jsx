a276391d2ed69aafed85cbbc56dec57b1b5ca66c
junaidahmed-One <junaidahmed882000@gmail.com>
2024-08-05 12:00:50 +0530

    memo, useCallback and its use-case

--- week-6/hooks-states-reconcilation/hooks-learn/src/App.jsx
+++ week-6/hooks-states-reconcilation/hooks-learn/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
 
 function App() {
 	const [excahnge1Data, setExcahnge1Data] = useState({});
@@ -29,14 +29,35 @@ function App() {
 
 	// useMemo() hook, saves re-calculating any operation. in the below example, cryptoReturns should not be calculated again after bankData updates,
 	//so using useMemo() hook. calculates only when excahnge1Data, excahnge2Data either of this changes.
-	const cryptoReturns = useMemo(() => {
-		console.log("under usememo");
-		return excahnge1Data.returns + excahnge2Data.returns;
-	}, [excahnge1Data, excahnge2Data]);
 
-	const incomeTax = (bankData.income + cryptoReturns) * 0.3;
+	// const cryptoReturns = useMemo(() => {
+	// 	console.log("under usememo");
+	// 	return excahnge1Data.returns + excahnge2Data.returns;
+	// }, [excahnge1Data, excahnge2Data]);
 
-	return <div>Hi your income tax returns are {incomeTax}</div>;
+	//const incomeTax = (bankData.income + cryptoReturns) * 0.3;
+
+	//useCallback() is used to memoize the function, and change only dependency array updates.
+	const calculateCryptogain = useCallback(
+		function () {
+			return excahnge1Data.returns + excahnge2Data.returns;
+		},
+		[excahnge1Data, excahnge2Data]
+	);
+
+	return (
+		<div>
+			<CryptoGainCalc
+				calculateCryptogain={calculateCryptogain}
+			></CryptoGainCalc>
+		</div>
+	);
 }
+
+// memo is used to skip re-render of component if its states are unchanged
+const CryptoGainCalc = memo(function ({ calculateCryptogain }) {
+	console.log("Child re-rendered");
+	return <div>Your crypto returns are {calculateCryptogain()}</div>;
+});
 
 export default App;
